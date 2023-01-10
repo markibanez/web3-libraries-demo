270730b892cd7ad9d58c2fa53326ab0ab63ebf77
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-10 10:34:55 +0800

    UI tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,12 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <div className={styles.description}>
-                    lol
-                </div>
+                {/* <div className={styles.description}>
+                    {`Injected (MetaMask)`}
+                    <button className={styles.button}>
+                        Connect Wallet
+                    </button>
+                </div> */}
             </main>
         </>
     );
